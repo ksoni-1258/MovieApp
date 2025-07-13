4c82f3438f734277b2bbc91720ddb93c299e4f0c
Kishan Soni <kishansoni@Kishans-MacBook.local>
2025-07-14 00:49:55 +0530

    DeepLinkHandle: Add info.plist for deeplink handler

--- MovieApp/App/DeepLinkManager.swift
+++ MovieApp/App/DeepLinkManager.swift
@@ -5,25 +5,31 @@
 //  Created by Kishan Soni on 11/07/25.
 //
 
+// MovieApp://movie?id=123
+// MovieApp://favorites
+
 import Foundation
 
 enum DeepLinkType {
     case movie(id: Int)
-    case profile
+    case favourite
 }
 
-class DeepLinkManager {
-    static let shared = DeepLinkManager()
+class DeepLinkManager: ObservableObject {
+    @Published var selectedTab: Int = 0
+    @Published var selectedMovie: Int? = nil
 
-    func handle(url: URL) -> DeepLinkType? {
-        let components = URLComponents(url: url, resolvingAgainstBaseURL: true)
-        guard let host = components?.host else { return nil }
-
-        if host == "movie", let id = Int(components?.queryItems?.first(where: { $0.name == "id" })?.value ?? "") {
-            return .movie(id: id)
-        } else if host == "profile" {
-            return .profile
+    func handle(url: URL) {
+        _ = URLComponents(url: url, resolvingAgainstBaseURL: true)
+        if url.host == "movie" {
+            if let components = URLComponents(url: url, resolvingAgainstBaseURL: false),
+               let queryItem = components.queryItems?.first(where: { $0.name == "id" }),
+               let movieIdString = queryItem.value,
+               let movieId = Int(movieIdString) {
+                    self.selectedMovie = movieId
+            }
+        } else if url.host == "favorites" {
+            selectedTab = 3
         }
-        return nil
     }
 }
